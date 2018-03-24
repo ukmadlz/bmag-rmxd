62dbcf3d4905084dd13a2f5eabf007d16e5e25d2
Mike Elsmore <mike@elsmore.me>
2018-03-24 11:53:38 +0000

    Convert the GSheet to hokey API

---
+++ app.js
@@ -0,0 +1,46 @@
+// Import Libraries
+import DotEnv from 'dotenv';
+import NodeSheets from 'node-sheets';
+import Hapi from 'hapi';
+
+// Load the ENV
+DotEnv.load({
+  silent: true
+});
+
+// Hapi
+const server = Hapi.server({
+  port: 4040,
+  host: 'localhost'
+});
+
+// Load the Google Sheet
+const GoogleSheets = new NodeSheets(process.env.GOOGLE_SHEET_ID);
+GoogleSheets.authorizeApiKey(process.env.GOOGLE_API_KEY)
+  .then(() => GoogleSheets.tables('Sheet1'))
+  .then(data => {
+    return data.rows.map(image => {
+      let newImageObject = {};
+      for (let key in image) {
+        if (image[key]) {
+          newImageObject[key] = image[key].stringValue;
+        }
+      }
+      return newImageObject;
+    });
+  })
+  .then(data => {
+    const init = async () => {
+      server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+          return data;
+        }
+      });
+      await server.start();
+      console.log(`Server running at: ${server.info.uri}`);
+    };
+    init();
+  })
+  .catch(console.error);
